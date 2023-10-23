fdc444fc34ee87660ab35a0cad82fde02384d701
gkholman <gkenholman+github@gmail.com>
2023-10-23 15:02:06 -0400

    First cut at server-based execution of build task

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$2" == "" ]
+if [ "$3" == "" ]
 then 
-echo Missing target directory and dateZ arguments
+echo Missing target directory, platform, and dateZ arguments
 exit
 fi
 
@@ -10,7 +10,36 @@ export UBLversion=2.3
 export UBLstage=csprd03
 
 export targetdir=$1
-export label=$2
+export platform=$2
+export label=$3
 export package=codelists
 export UBLversionStage=UBL-$UBLversion-$UBLstage
-bash make-UBL-code-lists.sh "$1" "$UBLversion" "$UBLstage" "$2"
+bash make-UBL-code-lists.sh
+
+# reduce GitHub storage costs by zipping results and deleting intermediate files
+pushd $targetdir
+if [ -f $package-$UBLversionStage-$label-archive-only.zip ]; then rm $package-$UBLversionStage-$label-archive-only.zip ; fi
+7z a $package-$UBLversionStage-$label-archive-only.zip $package-$UBLversionStage-$label-archive-only
+if [ -f $package-$UBLversionStage-$label.zip ]; then rm $package-$UBLversionStage-$label.zip ; fi
+if [ -d $package-$UBLversionStage-$label ]; then 7z a $package-$UBLversionStage-$label.zip $package-$UBLversionStage-$label ; fi
+popd
+
+if [ "$targetdir" = "target" ]
+then
+if [ "$platform" = "github" ]
+then
+if [ "$6" = "DELETE-REPOSITORY-FILES-AS-WELL" ] #secret undocumented failsafe
+then
+# further reduce GitHub storage costs by deleting repository files
+
+find . -not -name target -not -name .github -maxdepth 1 -exec rm -r -f {} \;
+
+mv $targetdir/$package-$UBLstage-$label-archive-only.zip .
+mv $targetdir/$package-$UBLstage-$label.zip .
+rm -r -f $targetdir
+
+fi
+fi
+fi
+
+exit 0 # always be successful so that github returns ZIP of results
--- make-UBL-code-lists.sh
+++ make-UBL-code-lists.sh
@@ -4,18 +4,19 @@ ant=utilities/ant
 saxon=utilities/saxon9he/saxon9he.jar
 xjparse=utilities/xjparse/xjparse.jar
 
-if [ ! -e "master-code-list-UBL-$2-$3.xml" ]
-then
-echo >build.prepare.$label.txt Missing input file: master-code-list-UBL-$2-$3.xml
-exit
-fi
-
 if [ ! -d $targetdir ]; then mkdir $targetdir ; fi
 cp -r . $targetdir/
 
 pushd $targetdir
+
+if [ ! -e "master-code-list-UBL-$UBLversion-$UBLstage.xml" ]
+then
+echo >build.console.$label.txt Missing input file: master-code-list-UBL-$UBLversion-$UBLstage.xml
+serverReturn=1
+else
+
 echo Building packaging script...
-java -jar $saxon -xsl:Crane-list2ant.xsl -s:master-code-list-UBL-$2-$3.xml -o:make-code-list.ant.xml "raw-uri-prefix=raw/" "intermediate-uri-prefix=work/" "output-uri-prefix=$3/" "bundle=$package-$UBLversionStage-$label" 2>build.prepare.$label.txt
+java -jar $saxon -xsl:Crane-list2ant.xsl -s:master-code-list-UBL-$UBLversion-$UBLstage.xml -o:make-code-list.ant.xml "raw-uri-prefix=raw/" "intermediate-uri-prefix=work/" "output-uri-prefix=$UBLstage/" "bundle=$package-$UBLversionStage-$label" 2>build.prepare.$label.txt
 serverReturn=$?
 if [ "$serverReturn" != "0" ]; then exit ; fi
 
@@ -24,18 +25,12 @@ java -Dant.home=$ant -classpath $xjparse:$ant/lib/ant-launcher.jar:$saxon:. org.
 serverReturn=$?
 if [ "$serverReturn" != "0" ]; then exit ; fi
 
+fi
+
 popd
+
 sleep 2
 if [ ! -d $targetdir/$package-$UBLversionStage-$label-archive-only/ ]; then mkdir $targetdir/$package-$UBLversionStage-$label-archive-only/ ; fi
-cp $targetdir/build.console.$label.txt $targetdir/$package-$UBLversionStage-$label-archive-only/
-cp $targetdir/build.prepare.$label.txt $targetdir/$package-$UBLversionStage-$label-archive-only/
+if [ -e $targetdir/build.console.$label.txt ]; then cp $targetdir/build.console.$label.txt $targetdir/$package-$UBLversionStage-$label-archive-only/ ; fi
 echo $serverReturn                    >$targetdir/$package-$UBLversionStage-$label-archive-only/build.exitcode.$label.txt
 touch                                  $targetdir/$package-$UBLversionStage-$label-archive-only/build.console.$label.txt
-
-# reduce GitHub storage costs by zipping results and deleting intermediate files
-pushd $targetdir
-if [ -f $package-$UBLversionStage-$label-archive-only.zip ]; then rm $package-$UBLversionStage-$label-archive-only.zip ; fi
-7z a $package-$UBLversionStage-$label-archive-only.zip $package-$UBLversionStage-$label-archive-only
-if [ -f $package-$UBLversionStage-$label.zip ]; then rm $package-$UBLversionStage-$label.zip ; fi
-7z a $package-$UBLversionStage-$label.zip $package-$UBLversionStage-$label
-popd
